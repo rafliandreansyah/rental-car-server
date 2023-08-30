234e331e433849fceee38955badd3f9d85283ae4
Rafli andreansyah <47078618+rafliandreansyah@users.noreply.github.com>
2023-08-30 21:37:17 +0700

    Initial commit

---
+++ src/main/java/com/rentalcar/server/RentalCarServerApplication.java
@@ -0,0 +1,13 @@
+package com.rentalcar.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalCarServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalCarServerApplication.class, args);
+    }
+
+}
